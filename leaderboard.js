5a9f15e1f9d17d28604e72a4208967ea7317b5e6
Diego de Souza <diegotrus7@gmail.com>
2015-05-23 21:22:22 -0500

    Created template helpers

--- leaderboard.js
+++ leaderboard.js
@@ -1,6 +1,14 @@
 PlayersList = new Mongo.Collection('players');
 
 if(Meteor.isClient){
+  Template.leaderboard.helpers({
+    'player': function(){
+       return "Some other text"
+    },
+    'otherHelperFunction': function(){
+       return "Some other function"
+    }
+  });
 }
 
 if(Meteor.isServer){
